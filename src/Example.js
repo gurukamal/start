f41946843d01366c91b11a121e7dc5427e2c4a2c
gurukamal <info.gurukamalchawla@gmail.com>
2019-03-27 12:29:14 +0530

    added a new component

---
+++ src/Example.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Example extends Component {
+  render(){
+return(
+	
+<div className="header-part">
+<h1>thi is me</h1>
+</div>
+    );
+  }
+}
+
+export default Example;
